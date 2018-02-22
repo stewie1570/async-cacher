3f55cfa044cae6944f7fcad63926de415db042ed
Anderson <Stewart.Anderson@titletec.com>
2018-02-22 09:29:23 -0700

    Implemented the cache.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-export var testFunc = () => true;
+export class Cache {
+    constructor({ timeProvider }) {
+        this.data = [];
+        this.timeProvider = timeProvider;
+    }
+
+    get({ dataSource, cacheKey, millisecondsToLive }) {
+        var cachedResult = this.data[cacheKey];
+        var adjust = ({ time, milliseconds }) => {
+            var newTime = new Date(time.valueOf());
+            newTime.setMilliseconds(milliseconds);
+            return newTime;
+        };
+        var getFromDataSource = () => {
+            var getData = dataSource();
+            this.data[cacheKey] = {
+                data: getData,
+                expiration: adjust({ time: this.timeProvider(), milliseconds: millisecondsToLive || 60000 })
+            };
+
+            return getData;
+        };
+        const hasCachedResult = cachedResult && this.timeProvider() < cachedResult.expiration;
+        
+        return hasCachedResult ? Promise.resolve(cachedResult.data) : getFromDataSource();
+    }
+}
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,70 @@
-import { testFunc } from '../src/index'
+import { Cache } from '../src/index'
 
-describe("Tests", () => {
-    it("should import and test systems under test", () => {
-        expect(testFunc()).toBeTruthy();
+describe("Cache", () => {
+    it("should return data from the source", async () => {
+        var dataSource = () => Promise.resolve("the data");
+        expect(await new Cache({ timeProvider: () => new Date() }).get({ dataSource })).toBe("the data");
+    });
+
+    it("should only request once per key", async () => {
+        var callCount = 0;
+        var dataSource = () => {
+            callCount++;
+
+            return Promise.resolve("the data");
+        };
+        var cache = new Cache({ timeProvider: () => new Date() });
+
+        expect(await cache.get({ dataSource, key: "key 1" })).toBe("the data");
+        expect(await cache.get({ dataSource, key: "key 1" })).toBe("the data");
+        expect(callCount).toBe(1);
+    });
+
+    it("should default expiration of cache keys to one minute", async () => {
+        var callCount = 0;
+        var dataSource = () => {
+            callCount++;
+
+            return Promise.resolve("the data");
+        };
+        var currentTime = new Date("01/01/2000 12:00 am");
+        var cache = new Cache({ timeProvider: () => currentTime });
+
+        expect(await cache.get({ dataSource, key: "key 1" })).toBe("the data");
+        var currentTime = new Date("01/01/2000 12:01 am");
+        expect(await cache.get({ dataSource, key: "key 1" })).toBe("the data");
+        expect(callCount).toBe(2);
+    });
+
+    it("should use configured cache key expiration", async () => {
+        var callCount = 0;
+        var dataSource = () => {
+            callCount++;
+
+            return Promise.resolve("the data");
+        };
+        var currentTime = new Date("01/01/2000 12:00 am");
+        var cache = new Cache({ timeProvider: () => currentTime });
+
+        expect(await cache.get({ dataSource, key: "key 1", millisecondsToLive: 1000 })).toBe("the data");
+        var currentTime = new Date("01/01/2000 12:00:01 am");
+        expect(await cache.get({ dataSource, key: "key 1" })).toBe("the data");
+        expect(callCount).toBe(2);
+    });
+
+    it("should not make another request while another request for the same key is in-flight", async () => {
+        var callCount = 0;
+        var dataSource = () => {
+            callCount++;
+
+            return Promise.resolve("the data");
+        };
+        var cache = new Cache({ timeProvider: () => new Date() });
+
+        const firstResult = cache.get({ dataSource, key: "key 1" });
+        const secondResult = cache.get({ dataSource, key: "key 1" });
+        expect(await firstResult).toBe("the data");
+        expect(await secondResult).toBe("the data");
+        expect(callCount).toBe(1);
     });
 });
